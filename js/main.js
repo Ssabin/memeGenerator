233ca75863e322781371f31ca2c353ea4029539b
Sahar Sabin <saharsabin14@gmail.com>
2016-12-06 13:36:02 +0200

    Meme edit template and Meme render have been added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
 'use strict';
-console.log('Hello Meme');
+const MEMES_IMGS = 7;
+
+var gMemes;
+
+
+function init(){
+    buildMemes();
+    renderMemes();
+}
+
+/**
+ * Builds model 
+ */
+function buildMemes(){
+    gMemes = [];
+    for (var i = 1; i <= MEMES_IMGS; i++) {
+        var meme = {
+            id: i,
+            url: '../assets/imgs/memes/'+ i +'.jpg',
+            keywords: ['happy','funny']
+        };
+        gMemes.push(meme);
+    }
+}
+
+/**
+ * Render memes
+ */
+
+function renderMemes(){
+    debugger;
+    var elMemesGallery__items = document.querySelector('.memes-gallery__items');
+    var strHTML = '';
+    gMemes.forEach(function(meme){
+        strHTML += '<li class="memes-gallery__hexagon meme'+ meme.id +'"><img src="../assets/imgs/memes/'+ meme.id +'.jpg" alt="meme" class="img-responsive meme__img"/></li>\n'
+    });
+    elMemesGallery__items.innerHTML = strHTML;
+}
