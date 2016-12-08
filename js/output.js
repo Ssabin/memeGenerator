9e38e2b8dcefa3390f10105d2fe9c0cc6d6303d0
Itai <itaikushmar@gmail.com>
2016-12-08 18:16:12 +0200

    list mode

--- js/output.js
+++ js/output.js
@@ -4,19 +4,34 @@
  * Render memes
  */
 
-function renderMemes(memes) {
+function renderMemes(memes, display) {
     var $memesGallery__memes = $('.memes-gallery');
     $memesGallery__memes.empty();
+    var $template = $('.memes-gallery__meme-template').find('.memes-gallery__meme');
     memes.forEach(function (meme) {
-        var $clone = $('.memes-gallery__meme-template').clone();
-        $clone.attr('class', 'memes-gallery__meme hexagon');
-        $clone.css('background-image', 'url(assets/imgs/memes/' + meme.id + '.jpg)');
+        var $clone = $template.clone();
+        $clone.find('.hexagon').css('background-image', 'url(assets/imgs/memes/' + meme.id + '.jpg)');
         $clone.on('click', function () {
             showGenerator(meme.url);
-        });;
-        $clone.toggle();
+        });
         $memesGallery__memes.append($clone);
+        $clone.find('.memes-gallery__keywords').text('Keywords: ' + meme.keywords);
     });
+}
+
+// toggles display modes: list and grid. also changes the button
+function toggleMemesList() {
+    $('.memes-gallery__keywords').toggle();
+    $('.memes-gallery').toggleClass('memes-gallery__list');
+    $('.memes-gallery__meme').toggleClass('memes-gallery__meme-list');
+    var $iconDisplay = $('.memes-display').find('i');
+    if($iconDisplay.hasClass('fa-list-ul')) {
+        $iconDisplay.removeClass('fa-list-ul');
+        $iconDisplay.addClass('fa-th');
+    }else{
+        $iconDisplay.removeClass('fa-th');
+        $iconDisplay.addClass('fa-list-ul');
+    }
 }
 
 /**
@@ -51,18 +66,18 @@ function backToGallery() {
  * This function renders keywords to DOM
  */
 
-function renderKeyWords(){
+function renderKeyWords() {
     var $searchKeyWords = $('.search-keywords');
-    for(var keyWord in gKeyWordsPopularity){
+    for (var keyWord in gKeyWordsPopularity) {
         var $keyWordDiv = $(document.createElement('div'));
         $keyWordDiv.text(keyWord);
         $keyWordDiv.addClass('search-keywords__keyword ' + keyWord);
-        $keyWordDiv.on('click' , function(e){
+        $keyWordDiv.on('click', function (e) {
             selectKeyWord(e.currentTarget.innerText);
         });
         $searchKeyWords.append($keyWordDiv);
 
-        setKeyWordFontSize(gKeyWordsPopularity[keyWord] , keyWord);
+        setKeyWordFontSize(gKeyWordsPopularity[keyWord], keyWord);
     }
 
 }
